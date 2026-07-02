e31fff0030d7c49f880a3587fac8f00a16cc56b6
samuelchimmy <samuelchimmy@gmail.com>
2026-07-02 01:08:44 +0100

    contract(celo): add MiniPay booster hooks to IOURegistry.sol (step 1/5)

---
+++ contracts/IOURegistry.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title IOURegistry
+ * @notice Stores mapping of social identities (Telegram, Discord, X) to Celo addresses.
+ * Used for walletless social payments in the Monipay ecosystem.
+ */
